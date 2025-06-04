8ae750c279c35c6fa7f80db5f458467a73235472
shahterjava <sergeicpetr6@gmail.com>
2025-06-04 14:54:29 +0700

    Добавлен файл CheckAge.java

---
+++ src/CheckAge.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CheckAge {
+    public static void main(String[] args) {
+        System.out.println("Введите ваш возраст:");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if(age<18){
+            System.out.println("Вы несовершеннолетний.");
+        }else System.out.println("Вы совершеннолетний.");
+    }
+}
