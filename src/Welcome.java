8ec8d42930838ff7c48b651d15d2f1e0520d3a1c
shahterjava <sergeicpetr6@gmail.com>
2025-06-04 14:32:36 +0700

    Добавлен файл Welcome.java

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        String name = "Dmitriy";
+        System.out.println("Привет, " + name + "! Добро пожаловать в мир Java!");
+    }
+}
