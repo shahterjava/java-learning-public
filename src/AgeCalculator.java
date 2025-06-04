b0a547c140e8148ad71d0f78c5a22c1371a3bcff
shahterjava <sergeicpetr6@gmail.com>
2025-06-04 14:39:12 +0700

    ÐÐДобавлен файл Calculator.java

---
+++ src/AgeCalculator.java
@@ -0,0 +1,8 @@
+public class AgeCalculator {
+    public static void main(String[] args) {
+        int year = 2025;
+        int yearOfBirth = 1966;
+        int ageCalculator = year - yearOfBirth;
+        System.out.println("Ваш возраст: " + ageCalculator + " лет.");
+    }
+}
