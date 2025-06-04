cd011149793164ed3af8f629272a4af54527502c
shahterjava <sergeicpetr6@gmail.com>
2025-06-04 14:48:49 +0700

    Добавлен файл AgeScanner.java

---
+++ src/AgeScanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AgeScanner {
+    public static void main(String[] args) {
+        System.out.println("Введите текущий год:");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        System.out.println("Введите ваш год рождения:");
+        Scanner scanner2 = new Scanner(System.in);
+        int yearOfBirth = scanner2.nextInt();
+        System.out.println("Ваш возраст: " + (year-yearOfBirth) + "!");
+
+    }
+}
