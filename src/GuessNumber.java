d2e8650210a347b73ad3610c48daa85c0d29276c
shahterjava <sergeicpetr6@gmail.com>
2025-06-05 09:39:09 +0700

    Добавлен ноÐвый файл ÐGuessNumber.java

---
+++ src/GuessNumber.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int value = rand.nextInt(0,11);
+        System.out.println("Программа загадала число, угадайте!");
+        Scanner scanner = new Scanner(System.in);
+        int userValue = scanner.nextInt();
+        if(userValue == value){
+            System.out.println("Поздравляю, вы угадали!");
+        }else{
+            System.out.println("Вы не угадали, попробуйте в следующий раз!");
+        }
+    }
+}
