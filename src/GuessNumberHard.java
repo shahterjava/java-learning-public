3a8fe344bbc35671ecfdd37cf770926a9c1f1b04
shahterjava <sergeicpetr6@gmail.com>
2025-06-05 09:57:31 +0700

    Добавлена новая вресиÑа новая версия игры GuessNumberHard.java

---
+++ src/GuessNumberHard.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumberHard {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int value = rand.nextInt(0,11);
+        System.out.println("Программа загадала число в интервале от 0 до 10," +
+                " попробуйте угадать это число!");
+
+        for (int i = 0; i <3 ; i++) {
+            System.out.println("Введите число!");
+            Scanner scanner = new Scanner(System.in);
+            int userValue = scanner.nextInt();
+            if(userValue == value){
+                System.out.println("Поздравляю, вы угадали число!");
+                break;
+            }else if(userValue<value) {
+                System.out.println("Повторите попытку! Загаданное число больше того," +
+                        " что вы ввели!");
+            }else{
+                System.out.println("Повторите попытку! Загаданное число меньше того," +
+                        " что вы ввели!");
+            }
+        }
+        System.out.println("Загаданное программой число было: "+value+ "!");
+    }
+}
