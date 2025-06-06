aa1d6f11dbacb8764e27d4a35fa622ff77bbbc83
shahterjava <sergeicpetr6@gmail.com>
2025-06-06 11:34:33 +0700

    ДоÑбавлена фуÐнкция MaxOfTwo

---
+++ src/MaxOfTwo.java
@@ -0,0 +1,8 @@
+public class MaxOfTwo {
+    public static void main(String[] args) {
+        System.out.println(max(7,4));
+    }
+    public static int max(int a, int b){
+        return Math.max(a, b);
+    }
+}
