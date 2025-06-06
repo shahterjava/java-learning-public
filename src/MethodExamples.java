775d10a7f1134abe03923245fe89648f99baa08e
shahterjava <sergeicpetr6@gmail.com>
2025-06-06 11:27:29 +0700

    Добавлен файл MethodExamples.java

---
+++ src/MethodExamples.java
@@ -0,0 +1,17 @@
+public class MethodExamples {
+    public static void main(String[] args) {
+        sayHello();
+        greetByName("Dmitriy");
+        sum(3,7);
+    }
+    public static void sayHello(){
+        System.out.println("Привет, пользователь.");
+    }
+    public static void greetByName(String name){
+        System.out.println("Привет, " + name +"!");
+    }
+    public static int sum(int a, int b){
+        System.out.println("Сумма: "+ (a+b));
+        return a+b;
+    }
+}
