288d2cc910dba9fbc882767ebfd37e0062e87377
shahterjava <sergeicpetr6@gmail.com>
2025-06-06 11:40:57 +0700

    Добавлен метод EvenChecker.java

---
+++ src/EvenChecker.java
@@ -0,0 +1,12 @@
+public class EvenChecker {
+    public static void main(String[] args) {
+        System.out.println(isEven(2));
+    }
+    public static boolean isEven(int number){
+        if(number%2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
