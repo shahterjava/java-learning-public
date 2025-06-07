fbeef3c04c321d2c362f750b15a26c64e8117a3e
shahterjava <sergeicpetr6@gmail.com>
2025-06-07 11:36:35 +0700

    ДÐобавлен клаÐсс Person.java

---
+++ src/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    private String name;
+    private int age;
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void sayHello(){
+        System.out.println("Привет, меня зовут: " +name+ ". Мне " + age + " лет.");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
