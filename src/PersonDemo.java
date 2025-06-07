20891740489232409fa0190c5bd1dc9e4b349e27
shahterjava <sergeicpetr6@gmail.com>
2025-06-07 11:38:44 +0700

    Добавлен раннер класса Person, PersonDemo.java

--- src/PersonDemo.java
+++ src/PersonDemo.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class PersonDemo {
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person person = new Person("Дмитрий", 23);
+        person.sayHello();
+        person.setName("Алексей");
+        person.sayHello();
+    }
 }
