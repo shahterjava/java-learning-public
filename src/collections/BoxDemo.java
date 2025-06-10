f228836ecc1a6f0e45a6c1b5fdac53303df1f385
shahterjava <sergeicpetr6@gmail.com>
2025-06-10 16:37:33 +0700

    Добавлена задача по работе с Generics

---
+++ src/collections/BoxDemo.java
@@ -0,0 +1,17 @@
+package collections;
+
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box<String> box = new Box<>();
+        box.setValue("Hello");
+        System.out.println(box.getValue());
+
+        Box<Integer> number = new Box<>();
+        number.setValue(221);
+        System.out.println(number.getValue());
+
+        Box<Task> taskBox = new Box<>();
+        taskBox.setValue(new Task("Приготовить завтрак!"));
+        System.out.println(taskBox.getValue());
+    }
+}
