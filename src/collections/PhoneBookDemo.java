29da6c02bdad2c67bd6e83a4e970c803c76733b9
shahterjava <sergeicpetr6@gmail.com>
2025-06-10 12:19:26 +0700

    Доьбавлена Ðобавлена новая версия PhoneBookDemo.java

--- src/collections/PhoneBookDemo.java
+++ src/collections/PhoneBookDemo.java
@@ -1,21 +1,28 @@
 package collections;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class PhoneBookDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String,String> phoneBook = new HashMap<>();
-        phoneBook.put("Дмитрий", "+7 900 332 1233");
-        phoneBook.put("Андрей", "+7 901 221 2322");
-        phoneBook.put("Федор", "+7 996 121 1311");
-        phoneBook.put("Мария", "+7 923 112 2311");
-        phoneBook.put("Елена", "+7 905 113 4552");
+        Map<String, List<String>> phoneBook = new HashMap<>();
+        phoneBook.putIfAbsent("Дмитрий", new ArrayList<>());
+        phoneBook.get("Дмитрий").add("+7 923 411 23 32");
+        phoneBook.get("Дмитрий").add("+7 906 424 23 22");
+        phoneBook.putIfAbsent("Андрей", new ArrayList<>());
+        phoneBook.get("Андрей").add("+7 911 425 23 22");
+        phoneBook.putIfAbsent("Федор", new ArrayList<>());
+        phoneBook.get("Федор").add("+7 945 414 23 22");
+        phoneBook.get("Федор").add("+7 943 414 23 22");
+        phoneBook.putIfAbsent("Мария", new ArrayList<>());
+        phoneBook.get("Мария").add("+7 932 624 23 22");
+        phoneBook.putIfAbsent("Елена", new ArrayList<>());
+        phoneBook.get("Елена").add("+7 916 424 23 22");
 
-        for (Map.Entry<String,String> map: phoneBook.entrySet()) {
+
+
+        for (Map.Entry<String,List<String>> map: phoneBook.entrySet()) {
             System.out.println(map);
         }
         System.out.print("Введите имя пользователя: ");
